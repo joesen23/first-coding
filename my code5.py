212c1e597595b4bb467da6e26d065be1d6b32563
joesen23 <reyesjoesen822@gmail.com>
2024-10-07 21:14:49 +0800

    late coding

---
+++ my code5.py
@@ -0,0 +1,9 @@
+workdays = ["Monday", "Tuesday", "Wednesday", "Thurdays", "Friday", "Saturday", "Sunday"]
+workdays.insert(2, "Thursdays")
+
+print(workdays)
+
+popped = workdays.pop(5)
+print("We just removed", popped)
+
+print(workdays)
